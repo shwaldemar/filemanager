475438670c2651cbc19f69bf873fe87e6c8f3f2d
Simon <shwaldemar@gmail.com>
2019-08-06 16:30:44 +0100

    filemanagerservice created

---
+++ src/main/java/com/codeclan/example/filemanagerservice/FilemanagerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.filemanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilemanagerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilemanagerserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/filemanagerservice/FilemanagerserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeclan.example.filemanagerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FilemanagerserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
